a4bfef061068260c7a636972ea3ef1d1b29cf1fc
Aygul Matyakubova <43939660+silentstar29@users.noreply.github.com>
2019-02-05 09:03:51 -0500

    Update js_app.js

--- static/js/js_app.js
+++ static/js/js_app.js
@@ -1,12 +1,14 @@
-// from data.js
+// Assign the data from `js_data.js` to a descriptive variable
 var tableData = data;
 
-// YOUR CODE HERE!
-
+// Get a reference to the table body
 var tbody= d3.select("tbody");
-
+// Create function buildTable to hold the table
 function buildTable(data){
     tbody.html("");
+    
+// Loop through data and append one table row `tr` for each object
+//Append a cell `td` to each row for each value
 data.forEach(function(ufo){
     var row =tbody.append("tr");
     row.append("td").text(ufo.datetime);
@@ -16,22 +18,23 @@ data.forEach(function(ufo){
     row.append("td").text(ufo.shape);
     row.append("td").text(ufo.durationMinutes);
     row.append("td").text(ufo.comments);
-
 });
 };
+//Call the function buildTable with tableData
 buildTable(tableData);
-
+// Select the submit button
 var submit= d3.select("#filter-btn")
-
     submit.on("click",function ufo(){
+// Prevent the page from refreshing
     d3.event.preventDefault();
-
+// Select the input element and get the raw HTML node
     var inputElement=d3.select("#datetime");
+// Get the value property of the input element
     var inputValue=inputElement.property("value");
 
     console.log(inputValue);
     console.log(tableData);
-    
+// Run if statement and assign a variable to hold filtered value  
     if (inputValue.length>0){
     var filteredData=tableData.filter(tableData=> tableData.datetime===inputValue);
     }
@@ -58,11 +61,11 @@ var submit= d3.select("#filter-btn")
 
     if (inputValue.length>0){
         var filteredData=tableData.filter(tableData=> tableData.shape===inputValue);}
-    
+        
+// Console log filtered data  
     console.log(filteredData);
 
-    //d3.selectAll("#form-control").remove();
-
+// Call buildTable function with filtered data
 buildTable(filteredData);
     
 });
